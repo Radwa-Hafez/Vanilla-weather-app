11a85b8f7404a4f7824a424387f19b4fe3791769
Radwa-Hafez <139390139+Radwa-Hafez@users.noreply.github.com>
2023-08-04 20:19:40 +0200

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+// Date function
 function formatDate(timeStamp) {
   let date = new Date(timeStamp);
   let hours = date.getHours();
@@ -20,7 +21,7 @@ function formatDate(timeStamp) {
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
-
+//API response function
 function displayTemperature(response) {
   let tempElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -41,8 +42,19 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
+//Search function
+function search(city) {
+  let apiKey = "69eeed235833e204804be8ef21541f75";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-let apiKey = "69eeed235833e204804be8ef21541f75";
-let city = "New York";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElemnt = document.querySelector("#city-input");
+  search(cityInputElemnt.value);
+}
+// control the search
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+search("Berlin");
